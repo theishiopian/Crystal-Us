37898773fe9aca9d479112454203d3ae57a75d5e
theishiopian <theishiopian@gmail.com>
2020-01-08 14:26:17 -0800

    github bot test 2

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 
 //global variable storage
+//use for things that everyone needs access to
 public static class GlobalVariables
 {
     public static Dictionary<string, Object> globalObjects = new Dictionary<string, Object>();//player can be stored here
