44b500613e42d84e74a153c798ff85bff54de5fd
theishiopian <theishiopian@gmail.com>
2020-01-08 14:10:59 -0800

    Hello world!

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello world");
     }
 
     // Update is called once per frame
