22ce38581e08df6aa23fdcb8623fccdc80ff2905
theishiopian <theishiopian@gmail.com>
2020-01-08 14:21:45 -0800

    Initial Boilerplate
    -Added event manager, refer to linked tutorial for usage
    -Added static class to store global variables. will be stored in dictionaries for ease of access, as well as to make it easy to add new ones. only use for things that need to be used by everything, otherwise just store locally.

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+    public static Dictionary<string, Object> globalObjects = new Dictionary<string, Object>();//player can be stored here
+    public static Dictionary<string, int> globalIntegers = new Dictionary<string, int>();//score etc
+}
