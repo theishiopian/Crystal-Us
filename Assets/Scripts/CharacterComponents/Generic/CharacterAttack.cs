efd20e89218e3880e799f57a525225a787ed3953
theishiopian <theishiopian@gmail.com>
2020-01-13 21:28:19 -0800

    Added attack component

---
+++ Assets/Scripts/CharacterComponents/Generic/CharacterAttack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CharacterAttack : MonoBehaviour, ICharacterComponent
+{
+    void Start()
+    {
+
+    }
+
+    public bool Attack(Vector2 direction, float attackPower)
+    {
+        Vector2 position = this.gameObject.transform.position;
+        RaycastHit2D hit = Physics2D.CircleCast(position,1,direction);
+
+        return true;
+    }
+}
