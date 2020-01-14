8539a3c65d7004814f99516398c2eea23b15ad2b
theishiopian <theishiopian@gmail.com>
2020-01-13 22:28:11 -0800

    Health system now works for enemies
    TODO: test for the player
    TODO: make an event on the other end of the triggerevent call in CharacterHealth (line 22)

--- Assets/Scripts/CharacterComponents/Generic/CharacterAttack.cs
+++ Assets/Scripts/CharacterComponents/Generic/CharacterAttack.cs
@@ -2,18 +2,27 @@
 
 public class CharacterAttack : MonoBehaviour, ICharacterComponent
 {
+    public string layerToHit;
+
+    private LayerMask mask;
     void Start()
     {
-
+        mask = LayerMask.GetMask(layerToHit);
     }
 
     public bool Attack(Vector2 direction, float attackPower)
     {
         Vector2 position = this.gameObject.transform.position;
-        RaycastHit2D hit = Physics2D.CircleCast(position,1,direction,2);
+        RaycastHit2D hit = Physics2D.CircleCast(position,1,direction,2,mask);
         Debug.DrawRay(position, direction, Color.blue,1);
+        CharacterHealth health = hit.collider.gameObject.GetComponent<CharacterHealth>();
+        if(hit && health != null)
+        {
+            Debug.Log("attacked: " + hit.collider.gameObject);
+            health.Damage(2 * Mathf.CeilToInt(attackPower));
+            return true;
+        }
 
-        
-        return hit;
+        return false;
     }
 }
---
+++ Assets/Scripts/CharacterComponents/Generic/CharacterHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour, ICharacterComponent
+{
+    public int health;
+    public bool isPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Damage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            if(isPlayer)
+            {
+                EventManager.TriggerEvent("death");
+            }
+            else
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("dead");
+        Destroy(this.gameObject);//todo: death effects? object pool?
+    }
+}
