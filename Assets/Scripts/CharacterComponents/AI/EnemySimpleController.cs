dbd222f0005fae1124292b4be848e70c2edfab80
theishiopian <theishiopian@gmail.com>
2020-01-21 13:26:13 -0800

    Particle Experiments

--- Assets/Scripts/CharacterComponents/AI/EnemySimpleController.cs
+++ Assets/Scripts/CharacterComponents/AI/EnemySimpleController.cs
@@ -104,12 +104,12 @@ public class EnemySimpleController : MonoBehaviour, ICharacterComponent, ICharac
     // Returns a random Vector2 in one of the four cardinal directions
     Vector2 RandomDirection()
     {
-        Debug.Log("called");
+        //Debug.Log("called");
         Vector2 direction = Random.insideUnitCircle.normalized;
         float angle = Mathf.Round(Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x) / 90) * 90; // change the 90s to 45s if you want 8 cardinal directions
         //float sin = Mathf.Sin(angle);
         //float cos = Mathf.Cos(angle);
-        Debug.Log(angle);
+        //Debug.Log(angle);
         switch (angle)//hack to get the direction right based on angle TODO: generic solution?
         {
             case 0: return new Vector2(1.0f, 0.0f);
