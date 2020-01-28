aafded3588c9c68573f8bb6ded87fd5b8fce7ab7
theishiopian <theishiopian@gmail.com>
2020-01-27 18:26:34 -0800

    Init TMP dialouge system
    -Imported TMP essentials
    -Start on dialouge

---
+++ Assets/Scripts/CharacterComponents/Generic/CharacterDialougeComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterDialougeComponent : MonoBehaviour, ICharacterComponent
+{
+    public string[] Lines;
+
+    private TextMeshPro speechBubble;
+
+    public void Start()
+    {
+        speechBubble = this.gameObject.GetComponentInChildren<TextMeshPro>();
+        speechBubble.SetText("");
+    }
+
+    public void PrintDialouge()
+    {
+
+    }
+}
