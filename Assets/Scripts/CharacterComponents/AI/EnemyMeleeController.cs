7fc47f40c9c72e2f8d325680a7adc8deb786895b
Gabriel Staffen <robotv56@live.com>
2020-01-29 13:27:22 -0800

    Added implementation of the ranged component into the melee controller.
    It might be a good idea to rename the EnemyMeleeController to just EnemyBasicController.

--- Assets/Scripts/CharacterComponents/AI/EnemyMeleeController.cs
+++ Assets/Scripts/CharacterComponents/AI/EnemyMeleeController.cs
@@ -10,12 +10,14 @@ public class EnemyMeleeController : MonoBehaviour, ICharacterComponent, ICharact
     public float followDistance;  // The distance the simple AI will follow the player before going back to the patrolArea
     public float startDelay;
     public float endDelay;
+    public float rangedPower;
 
     public Material[] effects;//temporary hurt effect TODO: replace with animation
 
     //component references
     private CharacterMoverComponent controller;
-    private CharacterMeleeComponent attack;
+    private CharacterMeleeComponent melee; // Will use melee if ranged is null
+    private CharacterRangedComponent ranged; // Will use ranged while not null
     private CharacterLevelComponent level;
     private new SpriteRenderer renderer;
 
@@ -24,11 +26,13 @@ public class EnemyMeleeController : MonoBehaviour, ICharacterComponent, ICharact
     private Vector2 moveDirection;      // Move direction
     private bool following = false;             // If the simple AI is following the player
 
+    
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterMoverComponent>();
-        attack = GetComponent<CharacterMeleeComponent>();
+        melee = GetComponent<CharacterMeleeComponent>();
+        ranged = GetComponent<CharacterRangedComponent>();
         level = GetComponent<CharacterLevelComponent>();
         renderer = GetComponent<SpriteRenderer>();
         patrolPoint = transform.position;
@@ -107,7 +111,7 @@ public class EnemyMeleeController : MonoBehaviour, ICharacterComponent, ICharact
 
             }
         }
-        else//attack code
+        else//meleecode
         {
             if(!attackStarted)
             {
@@ -121,7 +125,14 @@ public class EnemyMeleeController : MonoBehaviour, ICharacterComponent, ICharact
     {
         yield return new WaitForSeconds(startDelay);
         Vector2 direction = (player.transform.position - this.transform.position).normalized;
-        attack.Attack(direction);
+        if (ranged = null) // If the ranged component is null then use melee, otherwise use ranged.
+        {
+            melee.Attack(direction);
+        }
+        else
+        {
+            ranged.Attack(direction, rangedPower);
+        }
         renderer.material = effects[1];
         yield return new WaitForSeconds(endDelay);
         isAttacking = false;
