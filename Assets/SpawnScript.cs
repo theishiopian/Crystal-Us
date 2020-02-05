e9a0047d2ab39817d8dea4aa64c5cf7e47231646
Josh <jam678@gmail.com>
2020-02-05 14:50:29 -0800

    starting enemy spawner

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnScript : MonoBehaviour
+{
+    [SerializeField]
+        int leftUnits, rightUnits, upUnits, downUnits;
+    [SerializeField]
+        GameObject enemy;
+    private float distanceToPlayer;
+    //private GameObject enemySpawn;
+    
+
+    void Start()
+    {
+        GameObject spawnedEnemy = Instantiate(enemy, gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(GameObject.Find("Player").transform.position, transform.position);
+        /*switch(enemy)
+        {
+            case 0: enemySpawn = GameObject.Find("GelatinousCube");
+                break;
+            case 1: enemySpawn = GameObject.Find("Flumph");
+                break;
+            case 2: enemySpawn = GameObject.Find("DragonTurtle");
+                break;
+            case 3: enemySpawn = GameObject.Find("RockGolem");
+                break;
+        }*/
+
+    }
+}
