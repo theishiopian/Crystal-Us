2ec05f56bb237d5b30452d97ccdf7e9992afb6c3
theishiopian <theishiopian@gmail.com>
2020-02-09 23:50:43 -0800

    PlayerStats cleanup

--- Assets/Scripts/CharacterComponents/Player/PlayerStats.cs
+++ Assets/Scripts/CharacterComponents/Player/PlayerStats.cs
@@ -4,103 +4,19 @@ using UnityEngine;
 
 public static class PlayerStats
 {
-    private static int health=30, maxhealth=30, xp=0, level=1, levelUp=30;
-    private static bool hasKey=false, hasNecklace=false, hasSword=false;
+    public static int Health { get; set; } = 30;
 
-    public static int Health
-    {
-        get
-        {
-            return health;
-        }
-        set
-        {
-            health = value;
-        }
-    }
+    public static int Maxhealth { get; set; } = 30;
 
-    public static int Maxhealth
-    {
-        get
-        {
-            return maxhealth;
-        }
-        set
-        {
-            maxhealth = value;
-        }
-    }
+    public static int Xp { get; set; } = 0;
 
-    public static int Xp
-    {
-        get
-        {
-            return xp;
-        }
-        set
-        {
-            xp = value;
-        }
-    }
+    public static int Level { get; set; } = 1;
 
-    public static int Level
-    {
-        get
-        {
-            return level;
-        }
-        set
-        {
-            level = value;
-        }
-    }
+    public static int Levelup { get; set; } = 30;
 
-    public static int Levelup
-    {
-        get
-        {
-            return levelUp;
-        }
-        set
-        {
-            levelUp = value;
-        }
-    }
+    public static bool HasKey { get; set; } = false;
 
-    public static bool HasKey
-    {
-        get
-        {
-            return hasKey;
-        }
-        set
-        {
-            hasKey = value;
-        }
-    }
+    public static bool HasNecklace { get; set; } = false;
 
-    public static bool HasNecklace
-    {
-        get
-        {
-            return hasNecklace;
-        }
-        set
-        {
-            hasNecklace = value;
-        }
-    }
-
-    public static bool HasSword
-    {
-        get
-        {
-            return hasSword;
-        }
-        set
-        {
-            hasSword = value;
-        }
-    }
-
+    public static bool HasSword { get; set; } = false;
 }
